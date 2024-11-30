69923ad6955d9a4faaadacc3bf27a113bfa37ed6
James Wu <jameswlb01@gmail.com>
2024-11-30 11:52:35 +0800

    Create basic html struture, styles and js

--- myscript.js
+++ myscript.js
@@ -1,1 +1,19 @@
-// JS goes here
+// Array to store books
+const myLibrary = [];
+
+// Book Constructor
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read; //True or False 
+}
+
+// Add book to the list
+function addBookToLibrary(title, author, pages, read) {
+    const newBook = Book(title, author, pages, read);
+    myLibrary.push(newBook);
+} 
+
+// 
+
